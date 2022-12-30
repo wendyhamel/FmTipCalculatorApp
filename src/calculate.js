e71c07b35fba69fdd22f3d484a3238edba27c768
wendyhamel <whamel@it4web.nl>
2022-12-30 12:11:52 +0100

    Base setup

---
+++ src/calculate.js
@@ -0,0 +1,5 @@
+window.calculateJS = function() {
+    return {
+
+    }
+}
---
+++ src/tailwind-config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Space\\ Mono', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                'cyan-dark': 'hsl(183, 100%, 15%)',
+                'cyan': 'hsl(184, 14%, 56%)',
+                'cyan-light': 'hsl(185, 41%, 84%)',
+                'cyan-very-light': 'hsl(189, 41%, 97%)',
+                'white': 'hsl(0, 0%, 100%)',
+            },
+        }
+    }
+}

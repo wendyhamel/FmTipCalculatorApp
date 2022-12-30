6d8209aaa69a4f20dde299f4cdcc07737ffa113b
wendyhamel <whamel@it4web.nl>
2022-12-30 12:13:08 +0100

    Primary color added

--- src/tailwind-config.js
+++ src/tailwind-config.js
@@ -6,9 +6,10 @@ tailwind.config = {
         extend: {
             colors: {
                 'cyan-dark': 'hsl(183, 100%, 15%)',
-                'cyan': 'hsl(184, 14%, 56%)',
+                'cyan': 'hsl(172, 67%, 45%)',
+                'cyan-gray': 'hsl(184, 14%, 56%)',
                 'cyan-light': 'hsl(185, 41%, 84%)',
-                'cyan-very-light': 'hsl(189, 41%, 97%)',
+                'cyan-bleak': 'hsl(189, 41%, 97%)',
                 'white': 'hsl(0, 0%, 100%)',
             },
         }

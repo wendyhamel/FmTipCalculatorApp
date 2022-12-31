b9521b9081010f2fb3bcb2c12ce5fb56304b98c2
wendyhamel <whamel@it4web.nl>
2022-12-31 12:20:47 +0100

    working on js

--- src/calculate.js
+++ src/calculate.js
@@ -2,8 +2,37 @@ window.calculateJS = function() {
     return {
         bill: null,
         tip: null,
+        customTip: null,
+        tipTotal: null,
         people: null,
         tipAmount: 0,
         total: 0,
+
+        get totalTip() {
+            if (this.customTip) {
+                this.tip = null
+                this.tipTotal = this.customTip
+            } else if (this.tip) {
+                this.customTip = null
+                this.tipTotal = this.tip
+            } else if (!this.customTip && !this.tip) {
+                this.tipTotal = null
+            }
+        },
+
+        calculate() {
+            this.tipAmount = (this.bill / (100 * tipTotal)) / this.people
+            this.total = this.bill + this.tipAmount / this.people
+        },
+
+        reset() {
+            this.bill = null;
+            this.tip = null;
+            this.customTip= null;
+            this.tipTotal = null;
+            this.people = null;
+            this.tipAmount = 0;
+            this.total = 0;
+        }
     }
 }

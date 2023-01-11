7fe60dc5bfb0f3da95b7fa3bbe4890a4a64fd55f
wendyhamel <whamel@it4web.nl>
2023-01-11 15:03:47 +0100

    Calculations with Intel.NumberFormat for currency

--- src/calculate.js
+++ src/calculate.js
@@ -26,8 +26,11 @@ window.calculateJS = function() {
             this.tipPercentageForCalculation
 
             if (this.bill > 0 && this.tip && this.people > 0) {
-                this.tipAmount = Number((this.bill / 100) * this.tipPercentage / this.people).toFixed(2)
-                this.total = Number(this.bill / this.people).toFixed(2)
+                let tipPerPerson = (this.bill / 100) * this.tipPercentage / this.people
+                this.tipAmount = Number(new Intl.NumberFormat('en-US').format(tipPerPerson)).toFixed(2)
+                let subTotal = this.bill / this.people
+                let total = new Intl.NumberFormat('en-US').format(subTotal + tipPerPerson)
+                this.total = Number(total).toFixed(2)
             } else {
                 this.tipAmount = 0;
                 this.total = 0;

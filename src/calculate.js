b45c73e243d70453b84acf09a75c002181504045
wendyhamel <whamel@it4web.nl>
2024-09-24 12:13:31 +0200

    Refactored js

--- src/calculate.js
+++ src/calculate.js
@@ -5,41 +5,24 @@ window.calculateJS = function() {
         customTip: Alpine.$persist(null),
         selectedTip: Alpine.$persist(null),
         tipPercentage: Alpine.$persist(null),
-        tip: Alpine.$persist(null),
         people: Alpine.$persist(null),
         tipAmount: 0,
         total: 0,
 
-        get tipPercentageForCalculation() {
-            let tipPercentage
-            if (this.tip === 'preset') {
-                this.customTip = null;
-                tipPercentage = this.presetTips.filter((presetTip ) => { return presetTip === this.selectedTip })[0]
-            } else {
-                this.selectedTip = null;
-                tipPercentage = this.customTip
-            }
-            return this.tipPercentage = tipPercentage
-        },
-
-        get calculate() {
-            this.tipPercentageForCalculation
-
-            if (this.bill > 0 && this.tip && this.people > 0) {
+        calculate() {
+            this.tipAmount = 0;
+            this.total = 0;
+            if (this.bill > 0 && this.tipPercentage && this.people > 0) {
                 let tipPerPerson = (this.bill / 100) * this.tipPercentage / this.people
                 this.tipAmount = Number(new Intl.NumberFormat('en-US').format(tipPerPerson)).toFixed(2)
                 let subTotal = this.bill / this.people
                 let total = new Intl.NumberFormat('en-US').format(subTotal + tipPerPerson)
                 this.total = Number(total).toFixed(2)
-            } else {
-                this.tipAmount = 0;
-                this.total = 0;
             }
         },
 
         reset() {
             this.bill = null;
-            this.presetTip = null;
             this.customTip= null;
             this.selectedTip = null;
             this.tipPercentage = null;

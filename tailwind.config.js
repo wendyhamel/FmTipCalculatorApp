d348454231c8339ea8f40d4b8499c345e7bdd85f
wendyhamel <whamel@it4web.nl>
2024-09-24 12:26:10 +0200

    Replaced tailwind cdn by added tailwind to project.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/calculate.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Space\\ Mono', 'sans-serif']
+      },
+      colors: {
+        'cyan-dark': 'hsl(183, 100%, 15%)',
+        'cyan': 'hsl(172, 67%, 45%)',
+        'cyan-gray': 'hsl(184, 14%, 56%)',
+        'cyan-light': 'hsl(185, 41%, 84%)',
+        'cyan-bleak': 'hsl(189, 41%, 97%)',
+        'white': 'hsl(0, 0%, 100%)',
+      },
+    },
+  },
+  plugins: [],
+}
+
